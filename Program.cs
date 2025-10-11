36fe1b863ded29ba0037905fb75003f14613526a
YijiaWW <114932018+YijiaWW@users.noreply.github.com>
2025-10-11 13:10:14 -0400

    Update Program.cs
    Configure Entity Framework with In-Memory Database
    
    Configure Authentication and Authorization with JWT
    
    Enable Authentication and Authorization middleware

--- Program.cs
+++ Program.cs
@@ -1,8 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Midterm_EquipmentRental_Team2.Data;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+
+// Configure Entity Framework with In-Memory Database for simplicity
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseInMemoryDatabase("EquipmentRentalDb"));
+
+// Configure Authentication and Authorization
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("your_secret_key_here12345678901234")),
+        };
+    });
+builder.Services.AddAuthorization();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -17,6 +43,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// Enable Authentication and Authorization middleware
+app.UseAuthentication();
 
 app.UseAuthorization();
 
