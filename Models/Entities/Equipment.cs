403f6b24679c3a730a957ceeceb90b5392e477da
Johnstanley Ajagu <johnstanley.ajagu@gmail.com>
2025-10-11 14:52:35 -0400

    Moved the enums to the entity file

--- Models/Entities/Equipment.cs
+++ Models/Entities/Equipment.cs
@@ -1,32 +1,54 @@
-﻿//using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
-//namespace Midterm_EquipmentRental_Team2.Models
-//{
-//    public class Equipment
-//    {
-//        [Key]
-//        public int Id { get; set; }
+namespace Midterm_EquipmentRental_Team2.Models
+{
+    public class Equipment
+    {
+        // equipment category enums
+        public enum EquipmentCategory
+        {
+            HeavyMachinery,
+            PowerTools,
+            Vehicles,
+            Safety,
+            Surveying
+        }
 
-//        [Required]
-//        [MaxLength(100)]
-//        public string Name { get; set; } = string.Empty;
 
-//        [Required]
-//        public EquipmentCategory Category { get; set; }
+        // equipment conditon enums
+        public enum EquipmentCondition
+        {
+            New,
+            Excellent,
+            Good,
+            Fair,
+            Poor
+        }
 
-//        [Required]
-//        public EquipmentCondition Condition { get; set; }
 
-//        [MaxLength(255)]
-//        public string Description { get; set; } = string.Empty;
+        [Key]
+        public int Id { get; set; }
 
-//        public bool IsAvailable { get; set; } = true;
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; } = string.Empty;
 
-//        [MaxLength(20)]
-//        public string Status { get; set; } = "Available"; // Available, Rented, Maintenance, etc.
+        [Required]
+        public EquipmentCategory Category { get; set; }
 
-//        // Navigation property for rentals
-//        public ICollection<Rental> Rentals { get; set; } = new List<Rental>();
+        [Required]
+        public EquipmentCondition Condition { get; set; }
 
-//    }
-//}
+        [MaxLength(255)]
+        public string Description { get; set; } = string.Empty;
+
+        public bool IsAvailable { get; set; } = true;
+
+        [MaxLength(20)]
+        public string Status { get; set; } = "Available"; // Available, Rented, Maintenance, etc.
+
+        // Navigation property for rentals
+        public ICollection<Rental> Rentals { get; set; } = new List<Rental>();
+
+    }
+}
