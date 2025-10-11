0884de32443918ccc9b660b04510ab0e635becfb
Johnstanley Ajagu <johnstanley.ajagu@gmail.com>
2025-10-11 16:35:23 -0400

    updated equipment services

--- Services/EquipementService.cs
+++ Services/EquipementService.cs
@@ -1,6 +1,44 @@
-﻿namespace Midterm_EquipmentRental_Team2.Services
+﻿using Midterm_EquipmentRental_Team2.Models;
+using Midterm_EquipmentRental_Team2.Repositories;
+
+/// <summary>
+/// Equipment service class implementing the Equipment service interface crud operations
+/// </summary>
+namespace Midterm_EquipmentRental_Team2.Services
 {
-    public class EquipementService
+    public class EquipementService : IEquipementService
     {
+        private IEquipementRepository _repository;
+
+        public EquipementService(IEquipementRepository repository) 
+        {
+            _repository = repository;
+        }
+
+
+        public Equipment GetEquipmentById(int id)
+        {
+           return _repository.GetById(id);
+        }
+
+        public IEnumerable<Equipment> GetAllEquipments()
+        {
+            return _repository.GetAll().ToList();    
+        }
+
+        public void CreateEquipement(Equipment equipment)
+        {
+            _repository.Add(equipment);
+        }
+
+        public void DeleteEquipement(Equipment equipment)
+        {
+            _repository.Delete(equipment);
+        }
+
+        public void UpdateEquipement(Equipment equipment)
+        {
+            _repository.Update(equipment);
+        }
     }
 }
--- Services/IEquipementService.cs
+++ Services/IEquipementService.cs
@@ -1,6 +1,17 @@
-﻿namespace Midterm_EquipmentRental_Team2.Services
+﻿using Midterm_EquipmentRental_Team2.Models;
+
+namespace Midterm_EquipmentRental_Team2.Services
 {
+    /// <summary>
+    /// Defines the crud operations on the Equipment service decoupling the business logic
+    /// from the database implementation
+    /// </summary>
     public interface IEquipementService
     {
+        Equipment GetEquipmentById(int id);
+        IEnumerable<Equipment> GetAllEquipments();
+        void CreateEquipement (Equipment equipment);
+        void UpdateEquipement (Equipment equipment);
+        void DeleteEquipement (Equipment equipment);
     }
 }
