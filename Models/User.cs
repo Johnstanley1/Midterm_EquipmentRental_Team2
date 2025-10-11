d014f73b09e7073eb766be3019138d94ee72df5e
Johnstanley Ajagu <johnstanley.ajagu@gmail.com>
2025-10-11 13:38:00 -0400

    Created user and login request models

---
+++ Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace Midterm_EquipmentRental_Team2.Models
+{
+    // user model
+    public class User
+    {
+        public int Id { get; set; }
+        public string Usernmame { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+    }
+}
