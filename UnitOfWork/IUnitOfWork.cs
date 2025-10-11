aeadfe68a0ad455aaacc01d0b3ba45870664d1a8
Johnstanley Ajagu <johnstanley.ajagu@gmail.com>
2025-10-11 16:43:54 -0400

    updated equipment services

---
+++ UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using Midterm_EquipmentRental_Team2.Services;
+
+/// <summary>
+/// This cordinate the Equipment repository and treat it as a single transaction
+/// preventing partial updates and ensuring consistency
+/// </summary>
+
+namespace Midterm_EquipmentRental_Team2.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        IEquipementService Equipements { get; }
+        int complete();
+    }
+}
---
+++ UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,25 @@
+﻿using Midterm_EquipmentRental_Team2.Data;
+using Midterm_EquipmentRental_Team2.Services;
+
+/// <summary>
+/// Coordinates the Equipement repository operation. It also provides a single entry point to comit all changes   
+/// </summary>
+namespace Midterm_EquipmentRental_Team2.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private AppDbContext _appDbContext;
+        public IEquipementService Equipements { get; set; }
+
+        public UnitOfWork(AppDbContext appDbContext, IEquipementService equipementService)
+        { 
+            _appDbContext = appDbContext;
+            Equipements = equipementService;
+        }
+
+        public int complete()
+        {
+            return _appDbContext.SaveChanges();
+        }
+    }
+}
