aa893f750121ff7b55dcd498b120ebc5e1fe7199
Johnstanley Ajagu <johnstanley.ajagu@gmail.com>
2025-10-13 15:14:04 -0400

    fixed routing, created login screen

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import {LoginScreen} from './login-screen/login-screen';
+import {ErrorScreen} from './error-screen/error-screen';
+import {HomeScreen} from './home-screen/home-screen';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: "login", component: LoginScreen},
+  {path: "home", component: HomeScreen},
+  {path: "error", component: ErrorScreen},
+  {path: "", redirectTo: "/home", pathMatch: "full"},
+  {path: "*", component: ErrorScreen}
+];
