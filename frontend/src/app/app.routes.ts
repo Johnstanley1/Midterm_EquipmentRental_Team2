508ca45fc44aa76c0e5ddd52b91c0b7a72f2ae0a
Johnstanley Ajagu <johnstanley.ajagu@gmail.com>
2025-10-14 12:55:13 -0400

    created equipment services

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import {LoginScreen} from './login-screen/login-screen';
 import {ErrorScreen} from './error-screen/error-screen';
 import {HomeScreen} from './home-screen/home-screen';
+import {EquipmentScreen} from './equipment-screen/equipment-screen';
 
 export const routes: Routes = [
   {path: "login", component: LoginScreen},
   {path: "home", component: HomeScreen},
+  {path: "manage-equipment", component: EquipmentScreen},
   {path: "error", component: ErrorScreen},
   {path: "", redirectTo: "/login", pathMatch: "full"}
 ];
