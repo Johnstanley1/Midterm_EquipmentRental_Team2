c96cc2fc59382c0db9b13bc79f3f1aaf0fd65dfe
Johnstanley Ajagu <johnstanley.ajagu@gmail.com>
2025-10-13 23:51:34 -0400

    added a navigation bar

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,10 +1,11 @@
 import {Component, signal} from '@angular/core';
 import { RouterOutlet, RouterModule} from '@angular/router';
 import {CommonModule} from '@angular/common';
+import {NavBar} from './nav-bar/nav-bar';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, RouterOutlet, RouterModule],
+  imports: [CommonModule, RouterOutlet, RouterModule, NavBar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
