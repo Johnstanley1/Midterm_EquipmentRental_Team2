4b590a7e7c387e0215cbdcd2b1836b9f657c9a41
Johnstanley Ajagu <johnstanley.ajagu@gmail.com>
2025-10-14 11:17:20 -0400

    updated home page

--- frontend/src/app/home-screen/home-screen.ts
+++ frontend/src/app/home-screen/home-screen.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-home-screen',
-  imports: [],
+  imports: [
+    NgOptimizedImage,
+    RouterLink
+  ],
   templateUrl: './home-screen.html',
   styleUrl: './home-screen.css'
 })
