2e9bac7178b6038d8d34e842a27ef44801c062c1
YijiaWW <114932018+YijiaWW@users.noreply.github.com>
2025-10-14 13:39:39 -0400

    Add RentalController and implement RentalRepository
    Introduces RentalController and fully implements IRentalRepository and RentalRepository with methods for managing rentals, including retrieval, addition, update, and deletion. This sets up the backend structure for rental operations in the application.

--- backend/Repositories/IRentalRepository.cs
+++ backend/Repositories/IRentalRepository.cs
@@ -1,6 +1,19 @@
-﻿namespace Midterm_EquipmentRental_Team2.Repositories
+﻿using Midterm_EquipmentRental_Team2.Models;
+
+namespace Midterm_EquipmentRental_Team2.Repositories
 {
     public interface IRentalRepository
     {
+
+        IEnumerable<Rental> GetAll();
+        Rental? GetById(int id);
+        IEnumerable<Rental> GetByEquipmentId(int equipmentId);
+        IEnumerable<Rental> GetByCustomerId(int customerId);
+        IEnumerable<Rental> GetActiveByCustomerId(int customerId);
+        IEnumerable<Rental> GetCompletedByCustomerId(int customerId);
+        IEnumerable<Rental> GetOverdue();
+        void Add(Rental rental);
+        void Update(Rental rental);
+        void Delete(Rental rental);
     }
 }
