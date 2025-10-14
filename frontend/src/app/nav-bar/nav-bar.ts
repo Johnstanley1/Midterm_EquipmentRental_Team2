6b992747ea033cf81a8ce811e60a58bd469c2771
Johnstanley Ajagu <johnstanley.ajagu@gmail.com>
2025-10-13 23:54:27 -0400

    updated the navbar

--- frontend/src/app/nav-bar/nav-bar.ts
+++ frontend/src/app/nav-bar/nav-bar.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import {RouterLink} from '@angular/router';
+import {RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
   imports: [
-    RouterLink
+    RouterLink,
+    RouterLinkActive
   ],
   templateUrl: './nav-bar.html',
   styleUrl: './nav-bar.css'
