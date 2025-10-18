05fb2b29ece81cf643945be7346694c822a2cdc3
Johnstanley Ajagu <johnstanley.ajagu@gmail.com>
2025-10-18 00:14:52 -0400

    updated rental model

--- backend/Models/Entities/Rental.cs
+++ backend/Models/Entities/Rental.cs
@@ -15,30 +15,25 @@ namespace Midterm_EquipmentRental_Team2.Models
 
         public int Id { get; set; }
 
-        [Required]
-        public int CustomerId { get; set; }
-
-        [Required]
-        public int EquipmentId { get; set; }
-
-        [Required]
+        [Required(ErrorMessage = "Issue date is required")]
         public DateTime IssuedAt { get; set; }
 
+
+        [Required(ErrorMessage = "Due date is required")]
         public DateTime? DueDate { get; set; }
 
+
+        [Required(ErrorMessage = "Return date is required")]
         public DateTime? ReturnedAt { get; set; }
 
+
+        [Required(ErrorMessage = "Return note is required")]
         [MaxLength(255)]
         public string? ReturnNotes { get; set; }
 
         public Equipment.EquipmentCondition? ReturnCondition { get; set; }
-        public Customer? Customer { get; set; }
-        public Equipment? Equipment { get; set; }
         public IEnumerable<Equipment> Equipments { get; set; }
-
+        public IEnumerable<Customer> Customers { get; set; }
         public RentalStatus Status { get; set; }
-
-        //[MaxLength(20)]
-        //public string Status { get; set; } = "Active"; // 
     }
 }
