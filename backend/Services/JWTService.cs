12790a6d880dc2d4323327074214175aa48adec2
Johnstanley Ajagu <johnstanley.ajagu@gmail.com>
2025-11-04 22:16:46 -0500

    Add JWTService for token generation
    Introduces JWTService to generate JWT tokens using user claims and configuration settings. This service supports configurable token expiration and includes user roles in the token payload.

---
+++ backend/Services/JWTService.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Midterm_EquipmentRental_Team2.Services
+{
+    public class JWTService
+    {
+        private readonly IConfiguration _configuration;
+
+        public JWTService(IConfiguration configuration) 
+        {
+            _configuration = configuration;        
+        }
+
+        public string GenerateToken(ClaimsPrincipal user, TimeSpan? timeSpan = null ) {
+            var issuer = _configuration["Jwt: Issuer"];
+            var audience = _configuration["Jwt: Audience"];
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var now = DateTime.UtcNow;
+
+            var claims = new List<Claim>
+            {
+                new (ClaimTypes.NameIdentifier, user.FindFirstValue(ClaimTypes.NameIdentifier)),
+                new (ClaimTypes.Name, user.Identity.Name),
+                new (ClaimTypes.Email, user.FindFirstValue(ClaimTypes.Email))
+            };
+
+            foreach (var role in user.FindAll(ClaimTypes.Role).Select(c => c.Value).Distinct())
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+
+            var token = new JwtSecurityToken(
+                issuer: issuer,
+                audience: audience,
+                claims: claims,
+                notBefore: now,
+                expires: now.Add(timeSpan ?? TimeSpan.FromHours(1)),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
