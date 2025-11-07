ad9010c0585c4ebc22b913ca65fe7e4a5f794795
Johnstanley Ajagu <johnstanley.ajagu@gmail.com>
2025-11-07 04:35:16 -0500

    Fix route attribute for GetRentalEntityById endpoint
    Updated the HttpGet route from "{id}/" to "/{id}" in RentalController to ensure correct routing for fetching rental entities by ID.

--- backend/Controllers/RentalController.cs
+++ backend/Controllers/RentalController.cs
@@ -66,7 +66,7 @@ namespace Midterm_EquipmentRental_Team2.Controllers
 
 
         [Authorize(Roles = "Admin, User")]
-        [HttpGet("{id}/")]
+        [HttpGet("/{id}")]
         public ActionResult<RentalDTO> GetRentalEntityById(int id)
         {
             var rental = _unitOfWork.Rentals.GetRentalEntityById(id);
